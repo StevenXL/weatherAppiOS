f5aa32f2037f557e5f2ffc89bfe6ab27c10cafae
Steven Leiva <leiva.steven@gmail.com>
2016-08-13 15:08:00 -0400

    First attempt at a weather application.
    - Notice that App Transport Security requires HTTP requests to be made
      over a secure connection. I had to turn this off in the XCode /
    project settings.

--- index.ios.js
+++ index.ios.js
@@ -4,28 +4,75 @@
  * @flow
  */
 
+import validator from 'validator';
 import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
   Text,
+  TextInput,
   View
 } from 'react-native';
 
 class weatherAppiOS extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loaded: false,
+      description: '',
+      temperature: '',
+      zipCode: '',
+    };
+
+    this.fetchWeather = this.fetchWeather.bind(this);
+    this.handleTextChange = this.handleTextChange.bind(this);
+  }
+
+  fetchWeather() {
+    let url = `http://api.openweathermap.org/data/2.5/weather?zip=${this.state.zipCode},us&APPID=339bc502e738a05e187a870b4780626d`;
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((responseJson) => {
+        const description = responseJson.weather[0].description;
+        const temperature = responseJson.main.temp;
+
+        this.setState({description, temperature, loaded: true});
+      })
+      .catch((error) => console.error(error));
+  }
+
+  handleTextChange(text) {
+    this.setState({zipCode: text});
+  }
+
   render() {
+    let currentWeather;
+
+    if (this.state.loaded) {
+      currentWeather = (
+        <View>
+          <Text>It is currenty {this.state.description}</Text>
+          <Text>Current Temperature: {this.state.temperature}</Text>
+        </View>
+      );
+    }
+
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        {currentWeather}
+
+        <TextInput
+          autoFocus
+          keyboardType="number-pad"
+          maxLength={5}
+          onSubmitEditing={this.fetchWeather}
+          onChangeText={this.handleTextChange}
+          placeholder="Please enter a U.S. zip code..."
+          value={this.state.zipCode}
+          style={styles.userInput}
+        />
       </View>
     );
   }
@@ -35,18 +82,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  userInput: {
+    height: 20,
+    borderColor: 'gray',
+    borderWidth: 1,
   },
 });
 
